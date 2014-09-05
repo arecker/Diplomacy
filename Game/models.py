b4acd36d3a14847d89f68ddc482122c3e170387d
Alex Recker <alex@reckerfamily.com>
2014-09-05 16:09:59 -0500

    Such testing, requirements, and grace

--- Game/models.py
+++ Game/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import markdown2
 
 class Player(models.Model):
     name = models.CharField(max_length=200, verbose_name="Name")
     country = models.CharField(max_length=200, verbose_name="Country")
     email = models.CharField(max_length=200, verbose_name="Email")
+    image = models.CharField(max_length=500, verbose_name="Image URL")
     description = models.TextField(verbose_name="Description")
 
     def __unicode__(self):
@@ -14,6 +16,10 @@ class Update(models.Model):
     title = models.CharField(max_length=200, verbose_name="Title")
     season = models.CharField(max_length=200, verbose_name="Season")
     body = models.TextField(verbose_name="Body")
+
+
+    def body_to_html(self):
+        return markdown2.markdown(self.body)
 
 
     def __unicode__(self):
--- Game/tests.py
+++ Game/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from models import Update
 
-# Create your tests here.
+
+class TestUpdate(TestCase):
+	def setUp(self):
+		update = Update()
+		update.title = 'Test Update'
+		update.season = 'Test Season'
+		update.body = '**This should be bold**'
+		update.save()
+
+		self.update = update
+
+
+	def test_to_html(self):
+		expected = '<p><strong>This should be bold</strong></p>\n'
+		actual = self.update.body_to_html()
+		self.assertEqual(actual, expected)
