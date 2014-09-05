1028614f0b5aeeb2f1baed09e3a3554ef5b34ca9
Alex Recker <alex@reckerfamily.com>
2014-09-05 07:54:38 -0500

    added some modely stuff

--- Game/models.py
+++ Game/models.py
@@ -3,6 +3,16 @@ from django.db import models
 class Player(models.Model):
     name = models.CharField(max_length=200, verbose_name="Name")
     country = models.CharField(max_length=200, verbose_name="Country")
+    email = models.CharField(max_length=200, verbose_name="Email")
+    description = models.TextField(verbose_name="Description")
 
     def __unicode__(self):
         return self.name
+
+
+class Update(model.Model):
+    title = models.CharField(max_length=200, verbose_name="Title")
+
+
+    def __unicode__(self):
+        return self.title
